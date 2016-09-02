45c1229a3d999698e68af5add73c51c304bd28e8
lucasvenez <lucasvenez@gmail.com>
2016-09-02 18:52:00 -0300

    Add hibernate structure

---
+++ src/main/java/io/github/lucasvenez/ormclass/dao/DAO.java
@@ -0,0 +1,90 @@
+package io.github.lucasvenez.ormclass.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public abstract class DAO<E> {
+
+	protected EntityManager entityManager;
+	 
+    public DAO() {
+        entityManager = getEntityManager();
+    }
+ 
+    private EntityManager getEntityManager() {
+        
+    	EntityManagerFactory factory = 
+        		Persistence.createEntityManagerFactory("hibernate-example");
+    	
+        if (entityManager == null) {
+            entityManager = factory.createEntityManager();
+        }
+ 
+        return entityManager;
+    }
+ 
+    @SuppressWarnings("unchecked")
+    public List<E> findAll() {
+        return entityManager
+        		.createQuery("FROM Computer").getResultList();
+    }
+ 
+    public boolean persist(E object) {
+    	
+    	boolean result = true;
+    	
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(object);
+            entityManager.getTransaction().commit();
+        } catch (Exception ex) {
+        	ex.printStackTrace();
+            entityManager.getTransaction().rollback();
+            result = false;
+        }
+        
+        return result;
+    }
+ 
+    public boolean merge(E object) {
+    	
+    	boolean result = true;
+    	
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.merge(object);
+            entityManager.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            entityManager.getTransaction().rollback();
+            result = false;
+        }
+        
+        return result;
+    }
+ 
+    public boolean remove(E object) {
+    	
+    	boolean result = true;
+    	
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.remove(object);
+            entityManager.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            entityManager.getTransaction().rollback();
+            result = false;
+        }
+        
+        return result;
+    }
+
+	public abstract E getById(final Long id);
+    
+    public abstract boolean removeById(final Long id);
+    
+}
