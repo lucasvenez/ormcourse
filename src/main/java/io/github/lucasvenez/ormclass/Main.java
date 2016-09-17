afaa0dd2becdc94d4e72e90bec27e14872966717
lucasvenez <lucasvenez@gmail.com>
2016-09-16 22:31:13 -0300

    Add task 1

--- src/main/java/io/github/lucasvenez/ormclass/Main.java
+++ src/main/java/io/github/lucasvenez/ormclass/Main.java
@@ -1,5 +1,9 @@
 package io.github.lucasvenez.ormclass;
 
+import java.util.Date;
+
+import io.github.lucasvenez.ormclass.dao.OrderDAO;
+import io.github.lucasvenez.ormclass.model.Order;
 import io.github.lucasvenez.ormclass.model.Person;
 
 public class Main {
@@ -11,6 +15,18 @@ public class Main {
 		person.getBirthdate();
 		
 		person.getCity();
+		
+		Order order = new Order();
+		
+		order.setOrderDate(new Date());
+		
+		order.setOrderTime(new Date());
+		
+		//..
+
+		OrderDAO dao = new OrderDAO();
+		
+		dao.persist(order);
 	}
 
 }
