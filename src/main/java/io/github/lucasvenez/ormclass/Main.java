24d279eaeb1d5f6cd46dec8d55010240a139391f
cleytoncm <cleytoncm@gmail.com>
2016-09-23 15:32:42 -0300

    Iniciando o persist com o OrderItem.

--- src/main/java/io/github/lucasvenez/ormclass/Main.java
+++ src/main/java/io/github/lucasvenez/ormclass/Main.java
@@ -3,10 +3,13 @@ package io.github.lucasvenez.ormclass;
 import java.util.Date;
 
 import io.github.lucasvenez.ormclass.dao.OrderDAO;
+import io.github.lucasvenez.ormclass.model.Category;
 import io.github.lucasvenez.ormclass.model.City;
 import io.github.lucasvenez.ormclass.model.Country;
 import io.github.lucasvenez.ormclass.model.Order;
+import io.github.lucasvenez.ormclass.model.OrderItem;
 import io.github.lucasvenez.ormclass.model.Person;
+import io.github.lucasvenez.ormclass.model.Product;
 import io.github.lucasvenez.ormclass.model.State;
 
 public class Main {
@@ -23,6 +26,22 @@ public class Main {
 		
 		Order order = new Order();
 		order.setPerson(person);
+		order.setDate(new Date());
+		order.setTime(new Date());
+		
+		Category category = new Category("Bebidas");
+		
+		Product produto1 = new Product("Coca-Cola", 3.5, category);
+		Product produto2 = new Product("Guaraná", 3.5, category);
+		Product produto3 = new Product("Sukita", 3.5, category);
+		
+		OrderItem orderItem1 = new OrderItem(order, produto1, 10);
+		OrderItem orderItem2 = new OrderItem(order, produto2, 1);
+		OrderItem orderItem3= new OrderItem(order, produto3, 5);
+		
+		order.addOrderItem(orderItem1);
+		order.addOrderItem(orderItem1);
+		order.addOrderItem(orderItem1);
 		
 		OrderDAO orderDAO = new OrderDAO();
 		orderDAO.persist(order);
